5a5b213b7d712a1fdb492781e74d56b2cf8f3d6f
LukeBlueLOx <86468152+LukeBlueLOx@users.noreply.github.com>
2021-10-17 13:13:30 +0200

    Add files via upload

--- startupscript.sh
+++ startupscript.sh
@@ -1,9 +1,13 @@
-#! /bin/bash
+#!/bin/sh
 
-python3 /home/luke_blue_lox/clear_todaysum.py
+python3 /home/luke_green_player/Startup_Files/clear_todaysum.py
 
-python3 /home/luke_blue_lox/update_todaysum.py
+python3 /home/luke_green_player/Startup_Files/update_todaysum.py
 
-python3 /home/luke_blue_lox/clear.py
+python3 /home/luke_green_player/Startup_Files/clear.py
 
-python3 /home/luke_blue_lox/update.py
+python3 /home/luke_green_player/Startup_Files/update.py
+
+python3 /home/luke_green_player/Startup_Files/selenium_datawrapper.py
+
+# vim /etc/init.d/startupscript.sh
